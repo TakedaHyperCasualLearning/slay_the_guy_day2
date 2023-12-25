a6435518794886a928349dfb5de00bcc79b518be
TakedaNaomichi <takeda.naomichi@donuts.ne.jp>
2023-12-25 10:14:34 +0900

    基盤

---
+++ slay_the_guy_day2/Assets/Scripts/Main/Main.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    GameEvent gameEvent;
+    void Start()
+    {
+        gameEvent = new GameEvent();
+    }
+
+    void Update()
+    {
+    }
+}
